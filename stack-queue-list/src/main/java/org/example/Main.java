b052b8971d02b75d9b1b58aae8c3663e415e0528
JakubSadlowski <76012344+JakubSadlowski@users.noreply.github.com>
2023-10-25 11:03:32 +0200

    Made class CustomList and almost finished exercise001

--- stack-queue-list/src/main/java/org/example/Main.java
+++ stack-queue-list/src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
 package org.example;
 
-public class Main {
+import java.util.*;
 
+public class Main {
+    public static void main(String[] args){
+        CustomList list = new CustomList(10);
+
+        System.out.println("Sum of elements in list: " + list.getSum());
+        list.addAverageOfElementsToList();
+    }
 }
--- stack-queue-list/src/main/java/org/example/PairOfNumbers.java
+++ stack-queue-list/src/main/java/org/example/PairOfNumbers.java
@@ -11,12 +11,25 @@ public class PairOfNumbers {
         this.secondNumber = randomNumberGenerator();
     }
 
+    public PairOfNumbers(double firstNumber, double secondNumber) {
+        this.firstNumber = firstNumber;
+        this.secondNumber = secondNumber;
+    }
+
     public double getFirstNumber() {
         return firstNumber;
     }
 
     public double getSecondNumber() {
         return secondNumber;
+    }
+
+    public void setFirstNumber(double firstNumber) {
+        this.firstNumber = firstNumber;
+    }
+
+    public void setSecondNumber(double secondNumber) {
+        this.secondNumber = secondNumber;
     }
 
     private double randomNumberGenerator(){
